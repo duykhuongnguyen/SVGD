38106567ae832982edae42d4467b0ee89914087e
duynk13 <khuongduyprobg@gmail.com>
2022-03-28 16:30:25 +0700

    first commit

---
+++ inference.py
@@ -0,0 +1,15 @@
+import pickle
+import torch
+
+
+# Load data
+with open('results/test_data.pkl', 'rb') as f:
+    X_test = pickle.load(f)
+    X_test = torch.tensor(X_test, dtype=torch.float32)
+
+# Load model
+with open('checkpoints/net0_german_0.pickle', 'rb') as f:
+    model = pickle.load(f)
+
+
+print(model(X_test))
